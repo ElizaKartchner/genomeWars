ad2efd0cc17e2199e960c879673584488da0e24a
Eliza <eliza.kartchner@gmail.com>
2024-02-12 10:15:18 -0700

    add more gunc notes

--- GUNC/GUNC_notes.py
+++ GUNC/GUNC_notes.py
@@ -4,3 +4,12 @@
 # This is the GUNC Documentation Website 
 # https://grp-bork.embl-community.io/gunc/
 
+# How to download the GUNC Database
+# gunc download_db GUNC_DB 
+
+# Need to install diamond 2.0.4
+# https://gensoft.pasteur.fr/docs/diamond/2.0.4/2_Installation.html
+
+# Need to install prodigal 
+# conda install bioconda::prodigal
+# https://anaconda.org/bioconda/prodigal
